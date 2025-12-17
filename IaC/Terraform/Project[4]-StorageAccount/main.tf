4cb9aad9ec50f828fe8488c55379a1a337aefcff
mustafa sayed <mustafasayed.ali19@gmail.com>
2025-12-17 22:23:13 +0000

    Terraform Azure storage project-updated - v002

---
+++ IaC/Terraform/Project[4]-StorageAccount/main.tf
@@ -0,0 +1,59 @@
+terraform {
+
+  cloud { 
+    
+    organization = "esson_terra" 
+
+    workspaces { 
+      name = "project-terra" 
+    } 
+  } 
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = var.environment
+    project     = "terraform-storage"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ IaC/Terraform/Project[4]-StorageAccount/variables.tf
@@ -0,0 +1,16 @@
+variable "rg_name" {
+	default = "tf-essonn-rg"
+}
+
+variable "location" {
+	default = "uk south"
+}
+
+variable "storage_name" {
+	default = "tfessonnsa"
+}
+
+variable "environment" {
+	default = "storage-account"
+}
+
