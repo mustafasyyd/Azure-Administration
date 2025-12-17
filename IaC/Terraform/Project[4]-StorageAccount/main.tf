4a00f37783717756a56e59055e916628617e99ab
mustafa sayed <mustafasayed.ali19@gmail.com>
2025-12-17 23:04:10 +0000

    new terraform <storage_account>

--- IaC/Terraform/Project[4]-StorageAccount/main.tf
+++ IaC/Terraform/Project[4]-StorageAccount/main.tf
@@ -27,12 +27,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
+  name                       = var.storage_name
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  account_tier         	     = "Standard"
+  account_replication_type   = "LRS"
+  shared_access_key_enabled  = "true"
+  
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
 
--- IaC/Terraform/Project[4]-StorageAccount/variables.tf
+++ IaC/Terraform/Project[4]-StorageAccount/variables.tf
@@ -1,16 +1,24 @@
 variable "rg_name" {
-	default = "tf-essonn-rg"
+	default     = "tf-essonn-rg"
+	type        = string
+	description = "Resource group name"
 }
 
 variable "location" {
-	default = "uk south"
+	default     = "uk south"
+	type        = string
+        description = "Location name"
 }
 
 variable "storage_name" {
-	default = "tfessonnsa"
+	default     = "tfelsonmusa"
+        type        = string
+        description = "Storage Account Name"
 }
 
 variable "environment" {
-	default = "storage-account"
+	default     = "storage_account"
+        type        = string
+        description = "Environment Name"
 }
 
